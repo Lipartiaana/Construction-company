2ec05c8a097ae4df1716596fd8aa98902ee18547
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2024-03-17 18:25:10 +0400

    Rendering posts by date and adding responsive styles

--- JS/posts.js
+++ JS/posts.js
@@ -3,7 +3,7 @@ function renderPost(post) {
       <div class="post">
         <img src="${post.imageUrl}" alt="${post.title}">
         <div class="post-details">
-            <div class="psot-head">
+            <div class="post-head">
                 <h2>${post.title}</h2>
                 <p class="author">${post.author} ${post.date}</p>
             </div>
@@ -13,13 +13,40 @@ function renderPost(post) {
     `;
 }
 
+function cropContent(contentElement) {
+  let text = contentElement.textContent;
+  let limit = 300;
+
+  // If the content is long, it will show a part.
+  if (text.length > limit) {
+    const newText = text.slice(0, limit);
+    const readMore = document.createElement("a");
+    readMore.href = "#";
+    readMore.textContent = "Read more...";
+    readMore.classList.add("read-more");
+    return { newText, readMore };
+  }
+  return text;
+}
+
 fetch("../data.json")
   .then((response) => response.json())
   .then((data) => {
+    // Renders by date (latest to oldest)
+    data.posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
     const postsContainer = document.querySelector(".posts-container");
     data.posts.forEach((post) => {
-      const projectHTML = renderPost(post);
-      postsContainer.innerHTML += projectHTML;
+      let postContainer = document.createElement("div");
+      let postHTML = renderPost(post);
+      postContainer.innerHTML = postHTML;
+      postsContainer.appendChild(postContainer);
+      let contentElement = postContainer.querySelector(".content");
+      let { newText, readMore } = cropContent(contentElement);
+      contentElement.textContent = newText;
+      if (readMore) {
+        contentElement.appendChild(readMore);
+      }
     });
   })
   .catch((error) => console.error("Error fetching projects:", error));
