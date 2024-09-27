bcebb3d58a1ff05d6a24c30cabee8cfaaa84929c
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2024-09-27 23:22:18 +0400

    Adding gallery slider

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,17 +1,45 @@
 const galleryContainer = document.querySelector(".gallery-wrapper");
+const galleryModal = document.querySelector(".gallery-modal");
+
+const modalImage = document.querySelector(".modal-image");
+const close = galleryModal.querySelector(".close");
+const prevButton = galleryModal.querySelector(".prev-button");
+const nextButton = galleryModal.querySelector(".next-button");
 
 const galleryData = [];
 const imagesCount = 12;
+let currentImg = 0;
 
 for (let i = 0; i < imagesCount; i++) {
   galleryData.push(`../Assets/gallery-images/gallery-${i + 1}.jpg`);
 }
 
-galleryData.forEach((img) => {
+galleryData.forEach((img, index) => {
   const galeryImgLink = document.createElement("a");
-  const galleryImgHtml = `<a href="${img}">
-          <img src="${img}" alt="gallery image" />
-        </a>`;
+  const galleryImgHtml = `<img src="${img}" alt="gallery image" style="cursor:pointer" />`;
   galeryImgLink.innerHTML = galleryImgHtml;
+  galeryImgLink.onclick = function () {
+    openGallery(index);
+  };
   galleryContainer.appendChild(galeryImgLink);
+});
+
+function openGallery(index) {
+  currentImg = index;
+  modalImage.src = galleryData[currentImg];
+  galleryModal.style.display = "block";
+}
+
+close.addEventListener("click", () => {
+  galleryModal.style.display = "none";
+});
+
+nextButton.addEventListener("click", () => {
+  currentImg = (currentImg + 1) % imagesCount;
+  modalImage.src = galleryData[currentImg];
+});
+
+prevButton.addEventListener("click", () => {
+  currentImg = (currentImg - 1 + imagesCount) % imagesCount;
+  modalImage.src = galleryData[currentImg];
 });
